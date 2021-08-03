3a2b5efc2da897daeb0a7b42433cc8def7da72c7
Guilherme Laureano <guilhermeml001@gmail.com>
2021-08-03 01:27:33 -0300

    inicio alurakut

---
+++ pages/index.js
@@ -0,0 +1,54 @@
+import AvatarImage from '../src/components/avatarImage';
+import Box from '../src/components/box';
+import MainGrid from '../src/components/mainGrid';
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+
+function ProfileSideBar({ user }) {
+  return (
+    <Box>
+      <AvatarImage user={user} />
+    </Box>
+  );
+}
+
+function PeopleAvatar({ person }) {
+ return (
+   <li key={person}>
+    <a href={`/users/${person}`}>
+      <AvatarImage user={person} />
+      <span>{ person }</span>
+    </a>
+   </li>
+ );
+}
+
+export default function Home() {
+  const user = 'guilhermelaureano';
+  const people = [
+    'guilhermelaureano',
+  ];
+
+  return (
+    <>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea">
+          <ProfileSideBar user={user} />
+        </div>
+        <div className="welcomeArea">
+          <Box>
+            <h1 className="title" >Bem vindo(a)</h1>
+            <OrkutNostalgicIconSet />
+          </Box>
+        </div>
+        <div className="profileRelationsArea">
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">Pessoas da comunidade ({people.length})</h2>
+            <ul>{people.map((item) => <PeopleAvatar person={item} />)}</ul>
+          </ProfileRelationsBoxWrapper>
+        </div>
+      </MainGrid>
+    </>
+  );
+}
